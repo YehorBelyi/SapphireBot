9af66eebf3f426e31f5bc60709174a8c89007303
Yehor <Yehor@KOMPUTER>
2025-05-25 22:44:22 +0300

    Added command description to each command

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import asyncio
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import load_dotenv
+
 from handlers.user_private import user_private_router
+from common.bot_cmds_list import private
 
 load_dotenv()
 
@@ -16,6 +19,8 @@ dp.include_router(user_private_router)
 
 async def main() -> None:
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_my_commands(scope=BotCommandScopeAllPrivateChats())
+    await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 if __name__ == '__main__':
---
+++ common/bot_cmds_list.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='menu', description="Show built-in menu"),
+    BotCommand(command='help', description="Show built-in help"),
+]
