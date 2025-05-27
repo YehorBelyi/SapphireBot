4e136218ee2b0a2cb872117af8523425b499f08b
Yehor <Yehor@KOMPUTER>
2025-05-27 23:32:42 +0300

    Created asynchronious session maker

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import asyncio
 import os
 import logging
+from dotenv import load_dotenv
+from middlewares.database import DataBaseSession
+
+load_dotenv()
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.filters import CommandStart
 from aiogram.types import BotCommandScopeAllPrivateChats
 from aiogram.enums import ParseMode
-from dotenv import load_dotenv
+from database.engine import create_db, drop_db, session_maker
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
 from common.bot_cmds_list import private
-from middlewares.database import CounterMiddleware
-
-load_dotenv()
 
 ALLOWED_UPDATES = ['message, edited_message']
 
@@ -31,7 +32,26 @@ bot.my_admins_list = []
 # Added all handlers from modules
 dp.include_routers(user_private_router, user_group_router, admin_router)
 
+async def on_startup(bot):
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    # If old database wasn't dropped, new one won't be created, therefore data that exists there won't be modified
+    # This is how method create_db() works
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Shutting down')
+
 async def main() -> None:
+    # Doing some actions automatically, f.e related with database, whenever launches or shutdowns
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    # Allowing all handlers to use databse middleware, so we can easily do actions with database anywhere
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.delete_my_commands(scope=BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
--- middlewares/database.py
+++ middlewares/database.py
@@ -1,18 +1,21 @@
 from typing import Any, Awaitable, Callable, Dict
 from aiogram import BaseMiddleware
 from aiogram.types import Message, TelegramObject
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
+class DataBaseSession(BaseMiddleware):
+    def __init__(self, session_pool: async_sessionmaker):
+        # Transfering session to middleware
+        self.session_pool = session_pool
 
-# class CounterMiddleware(BaseMiddleware):
-#     def __init__(self) -> None:
-#         self.counter = 0
-#
-#     async def __call__(
-#         self,
-#         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-#         event: TelegramObject,
-#         data: Dict[str, Any]
-#     ) -> Any:
-#         self.counter += 1
-#         data['counter'] = self.counter
-#         return await handler(event, data)
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        # Instance 'session' allows to do requests towards database
+        async with self.session_pool() as session:
+            # Every router has access to THIS session now
+            data['session'] = session
+            return await handler(event, data)
