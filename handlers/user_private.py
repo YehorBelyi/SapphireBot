69c93d59798789c0e25f2d678d0d365ae61abb72
Yehor <Yehor@KOMPUTER>
2025-05-29 01:02:38 +0300

    Finished first step of creating menu processing fabric

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -6,44 +6,14 @@ from filters.chat_types import ChatTypeFilter
 from keyboards.reply import generate_keyboard
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.orm_query import orm_get_products
+from handlers.menu_processing import get_menu_content
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer("Welcome to Sapphire Shop",
-                         reply_markup=generate_keyboard(
-                             "Menu", "About", "Cart", "Order history", "Send number", "Payment methods",
-                             placeholder="Choose an option below",
-                             sizes=(2,2),
-                             request_contact=4
-                         ))
+async def start_cmd(message: types.Message, session: AsyncSession):
+    media, reply_markup = await get_menu_content(session, level=0, menu_name="main")
 
-@user_private_router.message(or_f(Command("menu"), (F.text.lower() == "menu")))
-async def echo(message: types.Message, session: AsyncSession):
-    await message.answer("Command menu")
-    for product in await orm_get_products(session):
-        await message.answer_photo(product.image,
-                                   caption=f"<b>{product.name}</b>\n"
-                                           f"<b>{product.description}</b>\n"
-                                           f"Price: {round(product.price, 2)}")
+    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
-@user_private_router.message(or_f(Command("about"), (F.text.lower() == "about")))
-async def echo(message: types.Message):
-    await message.answer("About our shop")
-
-@user_private_router.message(or_f(Command("payment"), (F.text.lower() == "payment methods")))
-async def echo(message: types.Message):
-    text = as_marked_section(
-        Bold("You can choose either:"),
-        "Credit card",
-        "After receiving product",
-        "Cryptocurrency",
-        marker="✅ "
-    )
-    await message.answer(text.as_html())
-
-@user_private_router.message(F.contact)
-async def contact(message: types.Message):
-    await message.answer(str(message.contact))
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,6 +1,34 @@
+from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+class MenuCallBack(CallbackData, prefix="menu"):
+    level: int
+    menu_name: str
+    # category: int | None = None
+    # page: int = 1
+    # product_id: int | None = None
+
+def get_user_main_btns(*, level:int, sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+    btns = {
+        "Shoes 👟":"catalog",
+        "Cart 🛒":"cart",
+        "About ℹ️":"about",
+        "Payment 💵":"payment",
+        "Shipping ⛴":"shipping",
+    }
+    for text, menu_name in btns.items():
+        if menu_name == "catalog":
+            keyboard.add(InlineKeyboardButton(text=text, callback_data=MenuCallBack(level=level+1, menu_name=menu_name).pack()))
+        elif menu_name == "cart":
+            keyboard.add(InlineKeyboardButton(text=text,
+                                              callback_data=MenuCallBack(level=3, menu_name=menu_name).pack()))
+        else:
+            keyboard.add(InlineKeyboardButton(text=text,
+                                              callback_data=MenuCallBack(level=level, menu_name=menu_name).pack()))
+
+    return keyboard.adjust(*sizes).as_markup()
 
 def get_callback_btns(
         *,
