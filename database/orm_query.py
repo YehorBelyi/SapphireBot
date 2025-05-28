6e34bcd223ddde99e805a61032898c2fbf33e999
Yehor <Yehor@KOMPUTER>
2025-05-29 02:55:33 +0300

    Partly implemented handlers for cart

--- database/orm_query.py
+++ database/orm_query.py
@@ -5,45 +5,6 @@ from sqlalchemy.orm import joinedload
 
 from database.models import Product, Cart, Category, Banner, User
 
-class Paginator:
-    def __init__(self, array: list | tuple, page: int=1, per_page: int=1):
-        self.array = array
-        self.per_page = per_page
-        self.page = page
-        self.len = len(self.array)
-        self.pages = math.ceil(self.len / self.per_page)
-
-    def __get_slice(self):
-        start = (self.page - 1) * self.per_page
-        stop = start + self.per_page
-        return self.array[start:stop]
-
-    def get_page(self):
-        page_items = self.__get_slice()
-        return page_items
-
-    def has_next(self):
-        if self.page < self.pages:
-            return self.page + 1
-        return False
-
-    def has_previous(self):
-        if self.page > 1:
-            return self.page - 1
-        return False
-
-    def get_next(self):
-        if self.page < self.pages:
-            self.page += 1
-            return self.get_page()
-        raise IndexError(f'Next page does not exist. Use has_next() to check before.')
-
-    def get_previous(self):
-        if self.page > 1:
-            self.page -= 1
-            return self.__get_slice()
-        raise IndexError(f'Previous page does not exist. Use has_previous() to check before.')
-
 # === Admin panel ===
 async def orm_add_product(session: AsyncSession, data: dict):
     obj = Product(
@@ -111,6 +72,27 @@ async def orm_get_user_carts(session: AsyncSession, user_id: int):
     query = select(Cart).where(Cart.user_id == user_id).options(joinedload(Cart.product))
     result = await session.execute(query)
     return result.scalars().all()
+
+async def orm_delete_from_cart(session: AsyncSession, user_id: int, product_id: int):
+    query = delete(Cart).where(Cart.user_id == user_id, Cart.product_id == product_id)
+    await session.execute(query)
+    await session.commit()
+
+async def orm_reduce_product_in_cart(session: AsyncSession, user_id: int, product_id: int):
+    query = select(Cart).where(Cart.user_id == user_id, Cart.product_id == product_id)
+    cart = await session.execute(query)
+    cart = cart.scalar()
+
+    if not cart:
+        return
+    if cart.quantity > 1:
+        cart.quantity -= 1
+        await session.commit()
+        return True
+    else:
+        await orm_remove_from_cart(session, user_id, product_id)
+        await session.commit()
+        return False
 
 # === Banner methods ===
 async def orm_add_banner_description(session: AsyncSession, data: dict):
--- handlers/menu_processing.py
+++ handlers/menu_processing.py
@@ -1,7 +1,9 @@
 from aiogram.types import InputMediaPhoto
-from database.orm_query import orm_get_banner, orm_get_categories, orm_get_products, Paginator
-from keyboards.inline import get_user_main_btns, get_user_catalog_btns, get_products_btns
+from database.orm_query import orm_get_banner, orm_get_categories, orm_get_products, orm_delete_product, \
+    orm_get_user_carts, orm_delete_from_cart, orm_reduce_product_in_cart, orm_add_to_cart
+from keyboards.inline import get_user_main_btns, get_user_catalog_btns, get_products_btns, get_user_cart
 from sqlalchemy.ext.asyncio import AsyncSession
+from utils.paginator import Paginator
 
 async def main_menu(session, level, menu_name):
     banner = await orm_get_banner(session, menu_name)
@@ -52,14 +54,63 @@ async def products(session, level, category, page):
 
     return image, kbds
 
+async def carts(session, level, menu_name, page, user_id, product_id):
+    if menu_name == "delete":
+        await orm_delete_from_cart(session, user_id, product_id)
+        if page > 1:
+            page -= 1
+    elif menu_name == "decrement":
+        is_cart = await orm_reduce_product_in_cart(session, user_id, product_id)
+        if page > 1 and not is_cart:
+            page -= 1
+    elif menu_name == "increment":
+        await orm_add_to_cart(session, user_id, product_id)
+
+    carts = await orm_get_user_carts(session, user_id)
+
+    if not carts:
+        banner = await orm_get_banner(session, "cart")
+        image = InputMediaPhoto(media=banner.image, caption=banner.description)
+
+        kbds = get_user_cart(
+            level=level,
+            page=None,
+            pagination_btns=None,
+            product_id=None,
+        )
+    else:
+        paginator = Paginator(carts, page=page)
+
+        cart = paginator.get_page()[0]
+
+        cart_price = round(cart.quantity*cart.product.price, 2)
+        total_price = round(sum(cart.quantity*cart.product.price for cart in carts), 2)
+        image = InputMediaPhoto(media=cart.product.image,
+                                caption=f"<b>{cart.product.name}</b>\n"
+                                        f"{cart.product.price}UAH x {cart.quantity} = {cart_price}UAH\n"
+                                        f"Product {paginator.page} out of {paginator.pages}\n"
+                                        f"Total Price: {total_price}\n")
+
+        paginations_btns = pages(paginator)
+        kbds = get_user_cart(level=level,
+                             page=page,
+                             pagination_btns=paginations_btns,
+                             product_id=cart.product.id)
+
+    return image, kbds
+
 async def get_menu_content(session: AsyncSession,
                            level: int,
                            menu_name: str,
                            category:int | None = None,
-                           page:int | None = None,):
+                           page:int | None = None,
+                           product_id:int | None = None,
+                           user_id:int | None = None,):
     if level == 0:
         return await main_menu(session, level, menu_name)
     elif level == 1:
         return await catalog(session, level, menu_name)
     elif level == 2:
         return await products(session, level, category, page)
+    elif level == 3:
+        return await carts(session, level, menu_name, page, user_id, product_id)
