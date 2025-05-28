0873cebebf664880df1aecbe7a30baa44f98afb3
Yehor <Yehor@KOMPUTER>
2025-05-29 01:48:38 +0300

    Finished 2 menu level

--- handlers/menu_processing.py
+++ handlers/menu_processing.py
@@ -1,6 +1,6 @@
 from aiogram.types import InputMediaPhoto
-from database.orm_query import orm_get_banner, orm_get_categories
-from keyboards.inline import get_user_main_btns, get_user_catalog_btns
+from database.orm_query import orm_get_banner, orm_get_categories, orm_get_products, Paginator
+from keyboards.inline import get_user_main_btns, get_user_catalog_btns, get_products_btns
 from sqlalchemy.ext.asyncio import AsyncSession
 
 async def main_menu(session, level, menu_name):
@@ -19,9 +19,47 @@ async def catalog(session, level, menu_name):
 
     return image, kbds
 
+def pages(paginator: Paginator):
+    btns = dict()
+    if paginator.has_previous():
+        btns["< Prev"] = "previous"
 
-async def get_menu_content(session: AsyncSession, level: int, menu_name: str):
+    if paginator.has_next():
+        btns["> Next"] = "next"
+
+    return btns
+
+async def products(session, level, category, page):
+    products = await orm_get_products(session, category_id=category)
+
+    paginator = Paginator(products, page=page)
+    # Returns first element of list
+    product = paginator.get_page()[0]
+
+    image = InputMediaPhoto(media=product.image,
+                            caption=f"<b>{product.name}</b>\n"
+                                    f"<b>{product.description}</b>\n"
+                                    f"Price: {round(product.price, 2)}\n"
+                                    f"Product {paginator.page} out of {paginator.pages}"
+                            )
+    paginations_btns = pages(paginator)
+
+    kbds = get_products_btns(level=level,
+                             category=category,
+                             page=page,
+                             pagination_btns=paginations_btns,
+                             product_id=product.id)
+
+    return image, kbds
+
+async def get_menu_content(session: AsyncSession,
+                           level: int,
+                           menu_name: str,
+                           category:int | None = None,
+                           page:int | None = None,):
     if level == 0:
         return await main_menu(session, level, menu_name)
     elif level == 1:
         return await catalog(session, level, menu_name)
+    elif level == 2:
+        return await products(session, level, category, page)
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -6,8 +6,8 @@ class MenuCallBack(CallbackData, prefix="menu"):
     level: int
     menu_name: str
     category: int | None = None
-    # page: int = 1
-    # product_id: int | None = None
+    page: int = 1
+    product_id: int | None = None
 
 def get_user_main_btns(*, level:int, sizes: tuple[int] = (2,)):
     keyboard = InlineKeyboardBuilder()
@@ -43,6 +43,41 @@ def get_user_catalog_btns(*, level:int, categories: list, sizes: tuple[int] = (2
                                           callback_data=MenuCallBack(level=level+1, menu_name=c.name, category=c.id).pack()))
 
     return keyboard.adjust(*sizes).as_markup()
+
+def get_products_btns(*,
+                      level: int,
+                      category: int,
+                      page: int,
+                      pagination_btns: dict,
+                      product_id: int,
+                      sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.add(InlineKeyboardButton(text="Go back",
+                                      callback_data=MenuCallBack(level=level - 1, menu_name="main").pack()))
+    keyboard.add(InlineKeyboardButton(text="Cart 🛒",
+                                      callback_data=MenuCallBack(level=3, menu_name="cart").pack()))
+    keyboard.add(InlineKeyboardButton(text="Order 📦",
+                                      callback_data=MenuCallBack(level=level, menu_name="add_to_cart", product_id=product_id).pack()))
+
+    keyboard.adjust(*sizes)
+
+    row = []
+    for text, menu_name in pagination_btns.items():
+        if menu_name == "next":
+            row.append(InlineKeyboardButton(text=text,
+                                            callback_data=MenuCallBack(level=level,
+                                                                       menu_name=menu_name,
+                                                                       category=category,
+                                                                       page=page+1).pack()))
+        elif menu_name == "previous":
+            row.append(InlineKeyboardButton(text=text,
+                                            callback_data=MenuCallBack(level=level,
+                                                                       menu_name=menu_name,
+                                                                       category=category,
+                                                                       page=page - 1).pack()))
+
+    return keyboard.row(*row).as_markup()
 
 def get_callback_btns(
         *,
